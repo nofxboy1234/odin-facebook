ed49e3982a99e56d3503346b29cee8e9fb8f0350
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-13 22:37:07 +0200

    Set up post.author -> user.posts
    - associations
    - migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_201052) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_13_203055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_201052) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -64,5 +66,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_201052) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
   add_foreign_key "profiles", "users"
 end
