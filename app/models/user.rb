8eb6dc2e1360f17ffa1ff225fe3846bc8b5f082b
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-13 21:56:10 +0200

    Revert "Comment out 'user.befriender = nil' in User.from_omniauth"
    This reverts commit 05dae2f2a558b9231489989d2ae4718ddf41d0fe.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      # user.befriender = nil
+      user.befriender = nil
       # user.name = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
 
