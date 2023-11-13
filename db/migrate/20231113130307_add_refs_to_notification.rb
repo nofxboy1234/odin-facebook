8f894c0f654455cae5a80778edb655ef25615eb8
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-13 21:57:24 +0200

    Revert "Generate and run migrations with references"
    This reverts commit 6849b8fe8750ebd23a4bcfae99cce73744bc5630.

--- db/migrate/20231113130307_add_refs_to_notification.rb
+++
@@ -1,7 +0,0 @@
-class AddRefsToNotification < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :notifications, :user,
-                  null: true,
-                  foreign_key: true
-  end
-end
--- db/migrate/20231113130833_add_refs_to_profile.rb
+++
@@ -1,7 +0,0 @@
-class AddRefsToProfile < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :profiles, :user,
-                  null: true,
-                  foreign_key: true
-  end
-end
