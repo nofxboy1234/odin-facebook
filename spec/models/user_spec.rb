b9a52b5f9c3fa7237a677a870569ec3c6eaf4688
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-15 11:55:27 +0200

    Add tests for User#posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) do
+    FactoryBot.create(:user)
+  end
+
+  let!(:post) do
+    FactoryBot.create(:post, content: 'hello', author: user)
+  end
+
+  describe '#posts' do
+    context 'when the user creates one post' do
+      before { user.posts << post }
+
+      it 'shows that post in the array' do
+        expect(user.posts).to include(post)
+      end
+
+      it 'has content: "hello" in the first post' do
+        first_post = user.posts.first
+        expect(first_post.content).to eql('hello')
+      end
+    end
+  end
 end
