a9dc357a5e37b4628d15ed9c2355da8edcc2eb42
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-16 15:57:14 +0200

    WIP: Test FriendRequest#accept

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,12 +1,12 @@
 class FriendRequest < ApplicationRecord
   belongs_to :user
   belongs_to :friendship, optional: true
-  
+
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
 
 
-  # def accept
+  def accept
 
-  # end
+  end
 end
--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe FriendRequest, type: :model do
-  
+  let!(:sender) do
+    create(:user)
+  end
+
+  let!(:receiver) do
+    create(:user)
+  end
+
+  let!(:friend_request) do
+    create(:friend_request)
+  end
+
+  before do
+    sender.send_friend_request(receiver, friend_request)
+    receiver.received_friend_requests.first.accept
+  end
+
+  context 'when a receiver accepts a friend request' do
+    it 'a Friendship is created' do
+      expect(sender.friendships.count).to eq(1)
+    end
+  end
 end
