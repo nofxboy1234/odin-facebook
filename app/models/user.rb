20615cfa8d1a11006a5b529e70c3b022a5b3c863
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-20 14:04:02 +0200

    Create DRY Mutual Friendships / Friends
    https://gist.github.com/jibiel/5c18d36b93891cced991791529fc1686
    Use scopes and modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
+  has_many :friendships,
+           ->(user) { FriendshipsQuery.both_ways(user_id: user.id) },
+           inverse_of: :user,
+           dependent: :destroy
 
-  # has_many :friendships, foreign_key: :user_id
-  # has_many :friends, through: :friendships, source: :friend_b
-
+  has_many :friends,
+           ->(user) { UsersQuery.friends(user_id: user.id, scope: true) },
+           through: :friendships
 
   # has_many :friend_requests_as_sender,
   #          class_name: 'FriendRequest',
@@ -14,18 +16,6 @@ class User < ApplicationRecord
   #          class_name: 'FriendRequest',
   #          foreign_key: :receiver_id
 
-  # has_many :friendships_as_friend_a,
-  #          class_name: 'Friendship',
-  #          foreign_key: :user_id
-
-  # has_many :friendships_as_friend_b,
-  #          class_name: 'Friendship',
-  #          foreign_key: :friend_id
-
-  # has_many :friend_as, through: :friendships_as_friend_b
-  # has_many :friend_bs, through: :friendships_as_friend_a
-
-
   # has_many :posts, foreign_key: :author_id
   # has_one :profile
   # has_many :notifications
@@ -33,7 +23,6 @@ class User < ApplicationRecord
 
   # has_many :likes
   # has_many :hearts, class_name: 'Like'
-
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -51,13 +40,5 @@ class User < ApplicationRecord
   # def send_friend_request(to_user, friend_request)
   #   sent_friend_requests << friend_request
   #   to_user.received_friend_requests << friend_request
-  # end
-
-  # def friendships
-  #   friendships_as_friend_a + friendships_as_friend_b
-  # end
-
-  # def friends
-  #   friend_as + friend_bs
   # end
 end
