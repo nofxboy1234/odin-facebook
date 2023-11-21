f864cb259bb6279a795b7e964f86779cb5914f7e
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-21 14:03:51 +0200

    Add :friendships_as_ and corresponding has_many :through associations and all_ methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,29 @@ class User < ApplicationRecord
 
   has_many :friends,
            lambda { |user|
-            where.not(id: user.id)
-            .joins("OR users.id = friendships.user_id")
+             where.not(id: user.id)
+                  .joins('OR users.id = friendships.user_id')
            },
            through: :friendships
+
+  has_many :friendships_as_user,
+           class_name: 'Friendship',
+           foreign_key: :user_id
+
+  has_many :friendships_as_friend,
+           class_name: 'Friendship',
+           foreign_key: :friend_id
+
+  # If creating 2 :friendships records for each friendship
+  # e.g. [{user_id: 1, friend_id: 2}, {user_id: 2, friend_id: 1}]
+  # then only call this method
+  has_many :user_friends,
+           through: :friendships_as_friend,
+           source: :user
+
+  has_many :friend_friends,
+           through: :friendships_as_user,
+           source: :friend
 
   has_many :friend_requests_as_sender,
            class_name: 'FriendRequest',
@@ -46,4 +65,17 @@ class User < ApplicationRecord
   #   sent_friend_requests << friend_request
   #   to_user.received_friend_requests << friend_request
   # end
+
+  # If creating 1 :friendships record for each mutual friendship
+  # e.g. [{user_id: 1, friend_id: 2}]
+  # then call these methods 
+  def all_friendships
+    reload
+    friendships_as_user + friendships_as_friend
+  end
+
+  def all_friends
+    reload
+    user_friends + friend_friends
+  end
 end
