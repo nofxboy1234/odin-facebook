453c91eff906e84a162155f45874678c9c765d42
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-27 13:08:58 +0200

    Add tests for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject!(:post) do
+    create(:post)
+  end
+
+  describe '#author' do
+    context 'when a post belongs to an author' do
+      let!(:author) do
+        create(:user)
+      end
+
+      before do
+        post.update!(author:)
+      end
+
+      it 'returns the user it belongs to' do
+        expect(post.author).to be(author)
+      end
+    end
+  end
+
+  describe '#comments' do
+    context 'when a post has comments on it' do
+      let!(:comments) do
+        post.comments << create_list(:comment, 2)
+      end
+
+      it 'returns the comments it has' do
+        expect(post.comments).to be(comments)
+      end
+    end
+  end
 end
