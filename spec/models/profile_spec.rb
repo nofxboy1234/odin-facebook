5f510bb82d565e7feaf6cae19ef1fea5acdb7e5d
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-27 14:27:37 +0200

    Add test for Profile#user

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#user' do
+    context 'when a profile belongs to a user' do
+      subject!(:profile) do
+        create(:profile)
+      end
+
+      let!(:user) do
+        create(:user)
+      end
+
+      before do
+        # user.profile = profile
+        profile.update!(user:)
+      end
+
+      it 'returns the user it belongs to' do
+        expect(profile.user).to eq(user)
+      end
+    end
+  end
 end
