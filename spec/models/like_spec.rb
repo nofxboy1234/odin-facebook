b9f0747322777c10760d8355afca86300d0b015b
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-27 11:29:10 +0200

    Create tests for Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#post' do
+    context 'when a post is liked' do
+      let!(:post) do
+        create(:post)
+      end
+
+      let!(:like) do
+        create(:like, post:)
+      end
+
+      it 'returns the post that was liked' do
+        expect(like.post).to be(post)
+      end
+    end
+  end
+
+  describe '#user' do
+    context 'when a user likes a post' do
+      let!(:user) do
+        create(:user)
+      end
+
+      let!(:like) do
+        create(:like, user:)
+      end
+
+      it 'returns the user that liked a post' do
+        expect(like.user).to be(user)
+      end
+    end
+  end
 end
