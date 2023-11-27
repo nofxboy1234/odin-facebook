cf0d9c4ca5d9694c02489051b0d5edd205745cc0
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-27 11:07:03 +0200

    Create tests for Friendship model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when user1 adds user2 as a friend' do
+    describe '#user' do
+      let!(:user1) do
+        create(:user)
+      end
+
+      let!(:user2) do
+        create(:user)
+      end
+
+      before do
+        user1.friends << user2
+      end
+
+      it 'returns user1' do
+        expect(Friendship.last.user).to eq(user1)
+      end
+    end
+
+    describe '#friend' do
+      let!(:user1) do
+        create(:user)
+      end
+
+      let!(:user2) do
+        create(:user)
+      end
+
+      before do
+        user1.friends << user2
+      end
+
+      it 'returns user2' do
+        expect(Friendship.last.friend).to eq(user2)
+      end
+    end
+  end
 end
