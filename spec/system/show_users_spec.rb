6733c3d8f348e0fbe529cc85c7109d0d76350e09
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-28 10:40:49 +0200

    Get ShowUsers system specs to pass
    login_as(create(:user))

--- spec/system/show_users_spec.rb
+++ spec/system/show_users_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "ShowUsers", type: :system do
-  # before do
-  #   driven_by(:rack_test)
-  # end
+  before(:each) do
+    # driven_by(:rack_test)
+    login_as(create(:user))
+  end
 
   describe 'index page' do
     it 'shows all users' do
+      p "Capybara.current_driver -- #{Capybara.current_driver}"
       visit users_path
       sleep(5)
       expect(page).to have_content('Users')
