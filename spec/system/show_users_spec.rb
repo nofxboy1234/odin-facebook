8874fdf88236930c7d7f17c3a175ebb29b518084
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-28 10:15:56 +0200

    Add ShowUsers system spec
    rails g rspec:system show_users

---
+++ spec/system/show_users_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "ShowUsers", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe 'index page' do
+    it 'shows all users' do
+      visit users_path
+      sleep(5)
+      expect(page).to have_content('Users')
+    end
+  end
+end
