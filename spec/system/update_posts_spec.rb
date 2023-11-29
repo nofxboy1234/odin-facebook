92fba32b11e7ceb2260338803214161f3dcaeb23
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-29 11:17:10 +0200

    Add system spec to test Updating a post with valid inputs

---
+++ spec/system/update_posts_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "UpdatePosts", type: :system do
+  let!(:existing_user) do
+    create(:user)
+  end
+
+  before(:each) do
+    # driven_by :selenium_chrome
+    login_as(existing_user)
+  end
+
+  scenario 'valid inputs' do
+    post = create(:post)
+    visit edit_post_path(post)
+    fill_in 'Content', with: 'Test Post'
+    click_on 'Update Post'
+    visit posts_path
+    expect(page).to have_content('Test Post')
+  end
+end
