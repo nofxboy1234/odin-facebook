b3c2929d27e8b5a8719659c0983f103368849f9f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-01 08:32:01 +0200

    Remove commented code

--- spec/system/delete_posts_spec.rb
+++ spec/system/delete_posts_spec.rb
@@ -21,15 +21,7 @@ RSpec.describe 'Deleting a post', type: :system do
       click_button 'Destroy this post'
     end
 
-    # sleep(0.1)
-    puts "\nposts_path: #{posts_path}"
-    
-    puts "current_path: #{current_path}"
     expect(page).to have_current_path(posts_path)
-    puts "current_path: #{current_path}"
-
-    # save_and_open_page
-    
     expect(page).not_to have_content('Remove me')
   end
 end
