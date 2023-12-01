c76cfe05c4ced91af15bb98dd3e18b896084a8f9
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-01 14:46:32 +0200

    Update system spec for creating a friend request

--- spec/system/create_friend_requests_spec.rb
+++ spec/system/create_friend_requests_spec.rb
@@ -1,32 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe 'CreateFriendRequests', type: :system do
-  let!(:existing_user) do
-    create(:user)
+  let!(:user1) do
+    create(:user, email: 'user1@example.com')
+  end
+
+  let!(:user2) do
+    create(:user, email: 'user2@example.com')
+  end
+
+  let!(:user3) do
+    create(:user, email: 'user3@example.com')
   end
 
   before(:each) do
-    # driven_by :selenium_chrome
-
-    create_list(:user, 2)
-    login_as(existing_user)
+    driven_by :selenium_chrome
   end
-
-  scenario 'valid inputs' do
+  
+  scenario 'log in as user1 with no friends' do
+    login_as(user1)
     visit users_path
-
-    user_to_add = User.second
-    user_id = user_to_add.id
+    sleep(3)
+    expect(page).not_to have_content('user1@example.com')
+    expect(page).to have_content('user2@example.com')
+    expect(page).to have_content('user3@example.com')
 
     accept_alert do
-      within("div#user_#{user_id}") do
+      within("div#user_#{user2.id}") do
         click_button 'Add Friend'
       end
     end
 
     expect(page).to have_current_path(friend_requests_path)
+
     expect(page).to have_content('Friend request was successfully created.')
-    expect(page).to have_content("sender: #{existing_user.email}")
-    expect(page).to have_content("receiver: #{user_to_add.email}")
+    expect(page).to have_content("sender: #{user1.email}")
+    expect(page).to have_content("receiver: #{user2.email}")
+    sleep(3)
+  end
+
+  scenario 'log in as user1 with friends [user2]' do
+    # create(:friend_request, sender: user1, receiver: user2)
+    # create(:friendship, user: user2, friend: user3)
+
+    login_as(user1)
+    visit users_path
+    sleep(3)
+    expect(page).not_to have_content('user1@example.com')
+    expect(page).to have_content('user2@example.com')
+    expect(page).to have_content('user3@example.com')
+
+    accept_alert do
+      within("div#user_#{user3.id}") do
+        click_button 'Add Friend'
+      end
+    end
+
+    expect(page).to have_current_path(friend_requests_path)
+
+    expect(page).to have_content('Friend request was successfully created.')
+    expect(page).to have_content("sender: #{user1.email}")
+    expect(page).to have_content("receiver: #{user3.email}")
+    sleep(3)
+
   end
 end
