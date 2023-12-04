445bf77f128793adfa2b177bff37ebf146feb852
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-04 19:17:55 +0200

    Fix lint issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params.merge({author: current_user}))
-    
+    @post = Post.new(post_params.merge({ author: current_user }))
+
     respond_to do |format|
       if @post.save
         format.html { redirect_to posts_url, notice: 'Post was successfully created.' }
