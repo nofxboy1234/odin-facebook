b2545dd597fa34c81fde35a04fe4778124086e09
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-04 17:21:18 +0200

    Remove author field from posts form and merge current_user as :author in PostsController

--- spec/system/create_posts_spec.rb
+++ spec/system/create_posts_spec.rb
@@ -12,20 +12,9 @@ RSpec.describe "Creating a post", type: :system do
   scenario 'valid inputs' do
     visit new_post_path
     fill_in 'Content', with: 'Goodbye world!'
-    fill_in 'Author', with: existing_user.id
     click_on 'Create Post'
 
     expect(page).to have_current_path(posts_path)
     expect(page).to have_content('Goodbye world!')
-  end
-
-  scenario 'invalid inputs' do
-    visit new_post_path
-    fill_in 'Content', with: ''
-    fill_in 'Author', with: ''
-    click_on 'Create Post'
-    
-    expect(page).to have_current_path(new_post_path)
-    expect(page).to have_content("Author can't be blank")
   end
 end
