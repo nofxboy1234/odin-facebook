993d6245c5ec13d473b79550da8b110be48c562b
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-04 12:28:42 +0200

    Update users#show view to show profile photo and list posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,9 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1 or /users/1.json
-  def show; end
+  def show
+    @profile_photo = profile_photo(@user)
+  end
 
   # GET /users/new
   def new
