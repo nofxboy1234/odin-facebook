4d785e8ff2d21331b8b3d10d71cdfab725e9425b
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-04 19:59:10 +0200

    Use form.hidden_field instead of merging current_user into post_params in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params.merge({ author: current_user }))
+    @post = Post.new(post_params)
 
     respond_to do |format|
       if @post.save
