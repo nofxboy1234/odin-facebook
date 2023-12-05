e15289774b4c64c82757947e2a53056391e9ded8
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-05 12:32:21 +0200

    Add link to comment on a post

---
+++ spec/system/comment_on_post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Creating a post", type: :system do
+  # let!(:existing_user) do
+  #   create(:user)
+  # end
+
+  # before(:each) do
+  #   login_as(existing_user)
+  # end
+
+  # scenario 'valid inputs' do
+  #   visit new_post_path
+  #   fill_in 'Content', with: 'Goodbye world!'
+  #   click_on 'Create Post'
+
+  #   expect(page).to have_current_path(posts_path)
+  #   expect(page).to have_content('Goodbye world!')
+  # end
+end
