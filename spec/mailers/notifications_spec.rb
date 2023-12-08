b3af7cc83d41c373760b822f3db3baf06e482c6b
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-08 10:03:20 +0200

    Rename some system specs and update notifications mailer spec

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -2,17 +2,20 @@ require "rails_helper"
 
 RSpec.describe NotificationsMailer, type: :mailer do
   describe "sign_up" do
-    let(:mail) { NotificationsMailer.sign_up }
+    let(:mail) { NotificationsMailer.with(user: recipient).sign_up }
+
+    let!(:recipient) do
+      create(:user)
+    end
 
     it "renders the headers" do
       expect(mail.subject).to eq("Sign up")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([recipient.email])
+      expect(mail.from).to eq(["notifications@example.com"])
     end
 
     it "renders the body" do
       expect(mail.body.encoded).to match("Hi")
     end
   end
-
 end
